f77f8f8a375aca9e797aaea420b638cdaca26fbd
fr0sne <gabriel16david@hotmail.com>
2023-08-25 02:12:08 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const http = require("http")
+const mysql = require("mysql2")
+const { faker } = require("@faker-js/faker")
+
+
+var connection = mysql.createConnection({
+  host     : 'mysql',
+  user     : 'root',
+  password : 'd3safio-n0d3js',
+  database : 'desafio-nodejs-db'
+});
+
+connection.connect()
+
+const app = http.createServer(async (request, response) => {
+  if(request.method === 'GET' && request.url === '/'){
+    console.log(request.url)
+    connection.query("SELECT * FROM people", async (err, rows, fields) => {
+      const names = rows.map(i => i.nome);
+      const randomName = faker.person.firstName();
+      const randomNameLI = `<li>${randomName}</li>`
+      const storedNamesLIs = names.map(i => `<li>${i}</li>`).join("\n")
+      connection.query(`INSERT INTO people VALUES ('${randomName}')`)
+      return response.end(`
+        <h1>Full Cycle Rocks!!</h1>
+        ${randomNameLI}
+        ${storedNamesLIs}
+      `)
+    })
+  }
+})
+
+app.listen(3000)
---
+++ mysql/dump.sql
@@ -0,0 +1,4 @@
+USE desafio-nodejs-db;
+CREATE TABLE people (
+    nome VARCHAR(255) NOT NULL
+);
